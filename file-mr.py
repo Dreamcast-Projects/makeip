01f6b6698fdc3651bf4ee13bc4e1df8ca0337cde
Andy <andress.barajas@gmail.com>
2019-08-30 17:52:32 -0700

    Figuring out license

--- file-mr.py
+++ file-mr.py
@@ -3,19 +3,6 @@
 # GIMP Plug-in for the Sega Dreamcast MR file format
 # Copyright (C) 2019 by BBHoodsta
 #
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation; either version 3 of the License, or
-#   (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
 # Encoding based on logotools(pngtomr.c) source code by Andrew Kieschnick
 # http://napalm-x.thegypsy.com/andrewk/dc/
 # Decoding based on sbinducer(mrimage.pass) source code by SiZiOUS 
